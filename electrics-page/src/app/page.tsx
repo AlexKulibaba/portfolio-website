b6e027ce59188c0e18f5090f6bd28680ae954dd2
Alex <115558678+AlexKulibaba@users.noreply.github.com>
2023-11-21 05:40:13 +0000

    small changes, added some color and so on

--- electrics-page/src/app/page.tsx
+++ electrics-page/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="bg-white text-gray-700">
+      <main className="pt-[125px] bg-white text-gray-700">
         {/* Hero section with a large image, overlay text and contact form */}
         <section className="relative md:flex md:items-center h-96 md:h-96 lg:h-[600px]">
           <Image
@@ -21,10 +21,17 @@ export default function Home() {
             height={landingImage.height}
             className="absolute inset-0 w-full h-full object-cover"
           />
-          <div className="w-full z-10 absolute inset-0 bg-black bg-opacity-50 p-4 text-white text-center md:text-left md:relative flex-1">
-            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-6xl font-bold mb-4">
+          <div className="lg:w-[600px] w-full h-full z-10 absolute inset-0 bg-black bg-opacity-50 p-4 text-white items-center justify-center text-center md:text-left md:relative flex flex-col pt-10">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4 text-center">
               Your Best Local Electrician
             </h1>
+            <h2 className="w-96 text-2xl sm:text-3xl md:text-4xl lg:text-3xl font-normal mb-4">
+              Your Go-to Electricians for any Electrical, Air Conditioning or
+              Solar Services.
+            </h2>
+            <button className="p-4 mt-10 rounded-md bg-vibrant-dark font-bold">
+              Get a free Quote!
+            </button>
           </div>
           <div className="z-10 hidden lg:flex md:absolute lg:right-10 items-center justify-center md:w-[400px]">
             <ContactForm />
