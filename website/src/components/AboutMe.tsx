ffd89080963208ce108602286fcf199dc8f508b4
AlexKulibaba <alexander.kulibaba@rwth-aachen.de>
2024-02-08 09:18:08 +1000

    added Profile pic

--- website/src/components/AboutMe.tsx
+++ website/src/components/AboutMe.tsx
@@ -1,21 +1,24 @@
 import React from "react";
+import alex_headshot from "../Images/alex_headshot.jpeg";
+import Image from "next/image";
 
 const AboutMe = () => {
   return (
-    <div className="container mx-auto px-6 flex flex-wrap justify-between pt-12">
-      <div className="flex justify-end w-full border-0 border-black p-2 pt-4 ">
-        <div className="md:w-1/2">
+    <div className="container mx-auto md:px-32 px-4 flex flex-wrap justify-between py-12">
+      <div className="flex md:flex-row-reverse flex-col justify-between border-0 border-black p-2 pt-4 ">
+        <div className=" md:pl-12 md:w-2/3">
           <h1 className=" text-3xl font-400">- About Me -</h1>
-          <p className="pt-4 text-[20px]">
-            I’m a Computer Science Student originally from Aachen, Germany with
-            web development experience ranging from small startups and
+          <p className="pt-4 text-[20px] pb-4 md:pb-0">
+            I&#39;m a Computer Science Student originally from Aachen, Germany
+            with web development experience ranging from small startups and
             businesses to large companies. In 2023 I have moved to Sydney and
             have since worked in web development as well as marketing for small
             businesses and tradespeople, and with my backgound in Computer
-            Science and Trade I'm excited to apply my skills and gro your online
-            presence
+            Science and Trade I'm excited to apply my skills and grow your
+            online presence
           </p>
         </div>
+        <Image src={alex_headshot} alt="Image" className=" md:w-1/3 -z-10" />
       </div>
     </div>
   );
--- website/src/components/Projects.tsx
+++ website/src/components/Projects.tsx
@@ -3,8 +3,19 @@ import React from "react";
 const Projects = () => {
   return (
     <div className="container mx-auto px-6 flex flex-wrap justify-between">
-      <div className="h-[700px] w-full border-2 border-black p-2">
-        My Projects
+      <div className=" w-full border-2 border-black p-16 ">
+        <h1 className=" pt-8 text-4xl">- My Projects -</h1>
+        <div className="pt-4 w-1/2 text-[20px]">
+          {
+            "These are the Projects I already completed, some are just designs, some are online and being used right now (just like this one). Feel free to click and visit them yourself!"
+          }
+        </div>
+        <div className="pt-4 flex flex-col">
+          <div className="flex flex-row">
+            <div className="w-1/2 h-64 bg-slate-600"></div>
+            <div className="w-1/2 h-64 bg-green-400"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
